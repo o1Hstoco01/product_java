d2b3c99e0e657f7b3f58a8317a43ebdafe9e8c98
Henrique <henristoco@gmail.com>
2024-10-29 11:06:31 -0300

    adicionando entities e repositories

---
+++ src/main/java/br/fatec/product/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package br.fatec.product.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.fatec.product.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
+}
