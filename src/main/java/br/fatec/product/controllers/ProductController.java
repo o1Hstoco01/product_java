308f0d508d27ce2b2bb56fa97f8d7dcce85bfa59
Henrique <henristoco@gmail.com>
2024-11-12 10:02:22 -0300

    Criando delete, update e save

--- src/main/java/br/fatec/product/controllers/ProductController.java
+++ src/main/java/br/fatec/product/controllers/ProductController.java
@@ -3,13 +3,19 @@ package br.fatec.product.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.fatec.product.entities.Product;
 import br.fatec.product.services.ProductService;
+import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("products")
@@ -19,12 +25,31 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping
-    public List<Product> getProducts() {
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getProducts() {
+        return ResponseEntity.ok(service.getAllProducts());
         
     }
     @GetMapping("{id}")
-    public Product getProduct(@PathVariable long id){
-        return service.getProductById(id);
+    public ResponseEntity<Product> getProduct(@PathVariable long id){
+        return ResponseEntity.ok(service.getProductById(id));
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateProduct(@PathVariable long id, @RequestBody Product product){
+        service.update(product, id);
+        return ResponseEntity.ok().build();
+    }
+    
+    @PostMapping()
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product){
+        Product newProduct = service.save(product);
+        return ResponseEntity.created(null).body(newProduct);
+    }
+
 }
--- src/main/java/br/fatec/product/services/ProductService.java
+++ src/main/java/br/fatec/product/services/ProductService.java
@@ -21,4 +21,23 @@ public class ProductService {
             () -> new EntityNotFoundException("Produto não cadastrado")
         );
     }
+    public void delete (long id){
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        }
+        else{
+            throw new EntityNotFoundException("Produto não cadastrado");
+        }
+    }
+    public Product save(Product product){
+        return repository.save(product);
+    }
+    public void update(Product product, long id){
+        Product aux = repository.getReferenceById(id);
+        aux.setCategory(product.getCategory());
+        aux.setName(product.getName());
+        aux.setPrice(product.getPrice());
+
+        repository.save(aux);
+    }
 }
